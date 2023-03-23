f8de03c001af3e45147ddd93aeb828e851606c69
gazudouin <128098713+gazudouin@users.noreply.github.com>
2023-03-23 11:32:48 -0600

    Add files via upload
    programacion de la fórmula general

---
+++ chicharronera.py
@@ -0,0 +1,12 @@
+#Programa de la formula general
+#Definir los coeficientes
+a=7/8
+b=10
+c=-5
+#Discriminante
+dis=(b**2)-(4*a*c)
+#soluciones
+x1=((-b)+(dis**0.5))/(2*a)
+x2=((-b)-(dis**0.5))/(2*a)
+print(x1)
+print(x2)
